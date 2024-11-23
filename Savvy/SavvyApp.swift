f1161914bd15daf364452049af0117c3bf6fc36e
JM Arenas <rns.jey@gmail.com>
2024-11-23 17:52:21 +0000

    Initial Commit

---
+++ Savvy/SavvyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavvyApp.swift
+//  Savvy
+//
+//  Created by JM Arenas on 23/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SavvyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SavvyTests/SavvyTests.swift
@@ -0,0 +1,17 @@
+//
+//  SavvyTests.swift
+//  SavvyTests
+//
+//  Created by JM Arenas on 23/11/2024.
+//
+
+import Testing
+@testable import Savvy
+
+struct SavvyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
