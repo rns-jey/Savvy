a07db7935166fb2595bcd931193e3cf603b437a3
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2024-12-06 18:10:36 +0000

    Build main view model

---
+++ Savvy/Models/MainViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  MainViewModel.swift
+//  Savvy
+//
+//  Created by JM Arenas on 06/12/2024.
+//
+
+import FirebaseAuth
+import Foundation
+
+class MainViewModel: ObservableObject {
+    @Published var currentUserId: String = ""
+    private var handler: AuthStateDidChangeListenerHandle?
+    
+    init() {
+        self.handler = Auth.auth().addStateDidChangeListener { [weak self] _, user in
+            DispatchQueue.main.async {
+                self?.currentUserId = user?.uid ?? ""
+            }
+        }
+    }
+    
+    public var isSignedIn: Bool {
+        return Auth.auth().currentUser != nil
+    }
+}
